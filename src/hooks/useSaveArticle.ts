74e29dbf8dab3f6e39b3a1aac7583c216f51f682
GearHead87 <hosanulislam87@gmail.com>
2024-07-17 18:42:56 +0600

    Add save article using react query

--- src/hooks/useSaveArticle.ts
+++ src/hooks/useSaveArticle.ts
@@ -1,6 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { makeAPIRequest } from '../lib/restAPI';
 
-export async function saveArticle(title: string, content: string) {
-	const res = await makeAPIRequest<void>('POST', '/news.json', { title, content });
-	return res;
-}
+type ArticleProp = {
+	title: string;
+	content: string;
+};
+
+const useSaveArticle = () => {
+	const queryClient = useQueryClient();
+
+	const { mutateAsync, isPending } = useMutation({
+		mutationKey: ['add-article'],
+		mutationFn: async (newArticle: ArticleProp) => {
+			const res = await makeAPIRequest<void>('POST', '/news.json', newArticle);
+			return res;
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['articles'],
+			});
+		},
+	});
+
+	const saveArticle = async (title: string, content: string) => {
+		console.log('I am hitsss');
+		try {
+			await mutateAsync({ title, content });
+		} catch (e) {
+			console.log(e);
+		}
+	};
+	return {
+		saveArticle,
+		isLoading: isPending,
+	};
+};
+
+export default useSaveArticle;
--- src/pages/AddNewsPage/index.tsx
+++ src/pages/AddNewsPage/index.tsx
@@ -1,16 +1,18 @@
 import { FormEvent } from 'react';
 import AddNewsFrom from '../../components/shared/Forms/AddNewsForm';
 import { useNavigate } from 'react-router-dom';
-import { saveArticle } from '../../hooks/useSaveArticle';
+import useSaveArticle from '../../hooks/useSaveArticle';
 
 const AddNewsPage = () => {
 	const navigate = useNavigate();
-	const handleSubmit = (e: React.SyntheticEvent<FormEvent>) => {
+	const { saveArticle } = useSaveArticle();
+
+	const handleSubmit = async (e: React.SyntheticEvent<FormEvent>) => {
 		e.preventDefault();
 		const form = e.target;
 		const title = form.title?.value;
 		const content = form.content?.value;
-		saveArticle(title, content);
+		await saveArticle(title, content);
 		navigate('/');
 	};
 	return (
